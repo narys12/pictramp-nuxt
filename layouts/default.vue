2d0dab310b550448f3e17e8f6aee1f91f33641a6
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2017-07-28 16:13:01 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app light toolbar footer>
+    <v-toolbar class="white" light dense>
+      <v-toolbar-title>PicTramp</v-toolbar-title>
+      <v-text-field class="ml-4" prepend-icon="search" hide-details single-line></v-text-field>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mail</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>event_busy</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>account_circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
